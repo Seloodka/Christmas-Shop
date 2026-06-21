7abf356484d46ee35aee539ee5e46d6a6975bc01
Nikita Tikhonov <seloodka@gmail.com>
2026-06-21 19:32:58 +0300

    feat: add scroll prevention when open burger menu

--- JS/burger.js
+++ JS/burger.js
@@ -1,7 +1,11 @@
 const burgerIcon = document.querySelector(".burger-icon");
 const burgerMenu = document.querySelector(".burger-wrapper");
+const scrollWidth = window.innerWidth - document.documentElement.clientWidth;
 
 burgerIcon.addEventListener('click', () => {
   burgerIcon.classList.toggle("burger-open");
   burgerMenu.classList.toggle("burger-closed");
-})
+  document.body.classList.toggle("prevent-scroll");
+});
+
+console.log(window.innerWidth - document.body.clientWidth);
