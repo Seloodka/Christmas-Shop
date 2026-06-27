685bcf94148d74d4e6d240cd57fcdb66f4b528fb
Nikita Tikhonov <seloodka@gmail.com>
2026-06-27 15:26:45 +0300

    feat: remove scrollBar while opened burger menu

--- JS/burger.js
+++ JS/burger.js
@@ -1,11 +1,20 @@
 const burgerIcon = document.querySelector(".burger-icon");
 const burgerMenu = document.querySelector(".burger-wrapper");
 const scrollWidth = window.innerWidth - document.documentElement.clientWidth;
+const header = document.querySelector("header");
+const main = document.querySelector("main");
 
 burgerIcon.addEventListener('click', () => {
   burgerIcon.classList.toggle("burger-open");
   burgerMenu.classList.toggle("burger-closed");
   document.body.classList.toggle("prevent-scroll");
+
+  if (burgerIcon.classList.contains("burger-open")) {
+    document.body.style.marginRight = scrollWidth + "px";
+  } else {
+    document.body.style.marginRight = "0px"
+  }
+
 });
 
 console.log(window.innerWidth - document.body.clientWidth);
