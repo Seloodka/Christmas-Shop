9206fc8a20ac73adf57c96f5af833cac99922704
Nikita Tikhonov <seloodka@gmail.com>
2026-07-28 20:32:23 +0300

    refractor: now modal grab data from json

--- JS/giftModal.js
+++ JS/giftModal.js
@@ -1,4 +1,6 @@
-const getCardImage = (card) => {
+import { cardsArray } from './globalVars.js';
+
+const cloneCardImage = (card) => {
   return card.querySelector("img").cloneNode();
 }
 
@@ -6,91 +8,110 @@ const getCardName = (card) => {
   return card.querySelector(".card-title").textContent;
 }
 
-const getCardTag = (card) => {
-  return card.querySelector(".card-tag").textContent;
+const getCardDataFromName = (name) => {
+  return cardsArray.find((card) => card.name === name);
 }
 
-const setRatingAmount = (stat, scale) => {
-  const statRatingAmountElement = stat.querySelector(".rating-amount");
-  return statRatingAmountElement.innerHTML = `+${scale * 100}`;
+const createModalTextTemplate = (cardData) => {
+  return `
+  <span class="card-tag">${cardData.category}</span>
+  <div class="modal-card-text">
+    <h2 class="card-title">${cardData.name}</h2>
+    <p class="card-description">${cardData.description}</p>
+  </div>
+  `
 }
 
-const fillStatRatingScale = (stat, scale) => {
-  const ratingScale = stat.querySelectorAll("path"); 
+const createStatRatingSnowflakes = (rating) => {
+  const snowflakes = [];
+  
+  for (let i = 1; i <= 5; i++) {
+    let opacity = 1;
 
-  for (let i = 0; i < scale; i++) {
-    ratingScale[i].style.fillOpacity = "1";
+    if (i > rating) {
+      opacity = 0.1;
+    }
+
+    snowflakes.push(
+    `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M12.1959 9.88162L11.6482 9.56542L13.1158 9.17219L12.8732 8.26704L10.5005 8.90278L9.38146 8.25667C9.39689 8.17336 9.40538 8.08765 9.40538 7.99997C9.40538 7.91229 9.39692 7.82655 9.38146 7.74327L10.5005 7.09716L12.8732 7.7329L13.1158 6.82775L11.6482 6.43452L12.1959 6.11831L14.546 5.97725L14.8921 4.02063L13.0246 3.34203L11.7274 5.30677L11.1797 5.62297L11.5729 4.15545L10.6678 3.91293L10.032 6.28561L8.91226 6.93211C8.78247 6.82103 8.63242 6.73313 8.4683 6.67494V5.3828L10.2052 3.64586L9.5426 2.98325L8.46827 4.05755V3.42515L9.51792 1.32584L7.99976 0L6.48157 1.3259L7.53122 3.42521V4.05761L6.45689 2.98332L5.79429 3.64592L7.53119 5.38286V6.675C7.36708 6.73319 7.21702 6.82109 7.08724 6.93217L5.96746 6.28568L5.33171 3.91299L4.42656 4.15551L4.81979 5.62304L4.27213 5.30684L2.9749 3.34209L1.10742 4.02069L1.45349 5.97731L3.80362 6.11838L4.35128 6.43458L2.88375 6.82781L3.1263 7.73296L5.49898 7.09722L6.61807 7.74333C6.60264 7.82664 6.59414 7.91235 6.59414 8.00003C6.59414 8.08771 6.60261 8.17345 6.61807 8.25673L5.49898 8.90285L3.1263 8.2671L2.88375 9.17226L4.35128 9.56548L3.80362 9.88169L1.45349 10.0227L1.10742 11.9793L2.97493 12.6579L4.27216 10.6932L4.81985 10.377L4.42662 11.8445L5.33177 12.087L5.96752 9.71435L7.0873 9.06786C7.21708 9.17894 7.36714 9.26684 7.53125 9.32503V10.6172L5.79435 12.3541L6.45696 13.0167L7.53129 11.9424V12.5748L6.48163 14.6741L7.99983 16L9.51802 14.6741L8.46837 12.5748V11.9424L9.5427 13.0167L10.2053 12.3541L8.4684 10.6172V9.32503C8.63251 9.26684 8.78257 9.17894 8.91235 9.06786L10.0321 9.71435L10.6679 12.087L11.573 11.8445L11.1798 10.377L11.7275 10.6932L13.0247 12.6579L14.8922 11.9793L14.5462 10.0227L12.1959 9.88162Z"
+        fill="#FF4646" fill-opacity="${opacity}" />
+    </svg>`
+    )
   }
+
+  return snowflakes;
 }
 
-const resetStatRatingScale = (stat) => {
-  const ratingScale = stat.querySelectorAll("path"); 
+const createModalSuperpowerTemplate = (superpowers) => {
 
-  for (let i = 0; i < ratingScale.length; i++) {
-    ratingScale[i].style.fillOpacity = "0.1";
+  let html = ` 
+  <div class="card-stats">
+    <span class="stats-title">Adds superpowers to:</span>
+    <div class="stats-wrapper">`;
+  
+  for (let power in superpowers) {
+    const powerRating = parseInt(superpowers[power]) / 100;
+    
+    html += ` 
+    <div class="stat">
+      <span class="stat-name">${power}</span>
+      <div class="stat-rating-wrapper">
+        <span class="rating-amount">${superpowers[power]}</span>
+        <div class="rating-container">
+        ${createStatRatingSnowflakes(powerRating).join("")}
+         </div>
+      </div>
+    </div>`;
   }
-}
 
-const fillStats = (stats, statsRatings) => {
+  html += `
+    </div>
+  </div>`;
 
-  stats.forEach((stat, index) => {
-    const statRatingScale = statsRatings[index];
-
-    setRatingAmount(stat, statRatingScale);
-    fillStatRatingScale(stat, statRatingScale);
-  })
+  return html;
 }
 
 const clearModal = (modal, modalContent) => {
-  modalContent.image.firstElementChild.remove();
-  modalContent.stats.forEach((stat) => resetStatRatingScale(stat));
+  modalContent.imageContainer.firstElementChild.remove();
+  modalContent.contentWrapper.innerHTML  = "";
 }
 
-const cardDescription = {
-  work: {
-    description: "Uses console.log like a crystal ball to find any issue.",
-  },
-  health: {
-    description: "Gets 10,000 steps a day even while sitting at the computer.",
-  },
-  harmony: {
-    description: "Laughs at code errors like they’re jokes instead of getting angry.",
-  }
+const cardClickHandler = (event) => {
+  
+  const card = event.currentTarget;
+  const cardName =  getCardName(card);
+  const cardType = card.dataset.cardType;
+  const cardData = getCardDataFromName(cardName);
+
+  const modalText = createModalTextTemplate(cardData);
+  const modalSuperpowers = createModalSuperpowerTemplate(cardData.superpowers);
+
+
+  modalContent.imageContainer.append(cloneCardImage(card));
+  modalContent.contentWrapper.insertAdjacentHTML("afterbegin", modalText);
+  modalContent.contentWrapper.insertAdjacentHTML("beforeend", modalSuperpowers);
+
+  modal.dataset.cardType = cardType;
+
+
+  modal.showModal();
+
+  modal.style.marginTop = (document.documentElement.clientHeight - modal.clientHeight) / 2 + "px";
+  modal.style.marginLeft = (document.documentElement.clientWidth - modal.clientWidth) / 2 + "px";
 }
 
 const modal = document.querySelector(".card-modal");
+const cards = document.querySelectorAll(".card");
 
 const modalContent = {
-  image: modal.querySelector(".image-container"),
-  tag: modal.querySelector(".card-tag"),
-  name: modal.querySelector(".card-title"),
-  description: modal.querySelector(".card-description"),
-  stats: modal.querySelectorAll(".stat"),
-}
-
-const giftsStats = {
-  work: [5, 5, 2, 4],
-  health: [4, 3, 5, 4],
-  harmony: [3, 2, 5, 5]
+  imageContainer: modal.querySelector(".image-container"),
+  contentWrapper: modal.querySelector(".modal-text-wrapper"),
 }
 
 cards.forEach((card) => {
-  card.addEventListener("click", () => {
-    const cardType = card.dataset.cardType;
-
-    modal.dataset.cardType = cardType;
-    modalContent.image.append(getCardImage(card));
-    modalContent.name.innerHTML = getCardName(card);
-    modalContent.tag.innerHTML = getCardTag(card);
-    modalContent.description.innerHTML = cardDescription[cardType].description;
-
-    fillStats(modalContent.stats, giftsStats[cardType]);
-
-    modal.showModal();
-
-    modal.style.marginTop = (document.documentElement.clientHeight - modal.clientHeight) / 2 + "px";
-    modal.style.marginLeft = (document.documentElement.clientWidth - modal.clientWidth) / 2 + "px";
-  })
+  card.addEventListener("click", cardClickHandler);
 })
 
 modal.querySelector(".modal-close").addEventListener("click", () => {
