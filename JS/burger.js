01e0df7eed8a52a1421403d72a3fd5199c0722b2
Nikita Tikhonov <seloodka@gmail.com>
2026-07-30 18:18:26 +0300

    refractor: burger script improvement

--- JS/burger.js
+++ JS/burger.js
@@ -1,15 +1,14 @@
 const burgerIcon = document.querySelector(".burger-icon");
 const burgerMenu = document.querySelector(".burger-wrapper");
-const scrollWidth = window.innerWidth - document.documentElement.clientWidth;
 
-burgerIcon.addEventListener('click', () => {
+const burgerMenuClickHandler = (event) => {
+  const scrollWidth = window.innerWidth - document.documentElement.clientWidth;
+
   burgerIcon.classList.toggle("burger-open");
   burgerMenu.classList.toggle("burger-closed");
+  
   document.body.classList.toggle("prevent-scroll");
+  document.body.style.marginRight = scrollWidth + "px";
+}
 
-  if (burgerIcon.classList.contains("burger-open")) {
-    document.body.style.marginRight = scrollWidth + "px";
-  } else {
-    document.body.style.marginRight = "0px"
-  }
-});
+burgerIcon.addEventListener('click', burgerMenuClickHandler);
